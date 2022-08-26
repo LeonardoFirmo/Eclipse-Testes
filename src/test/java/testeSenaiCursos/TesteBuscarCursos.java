061bfe44f2f579c28cd41f9d90a274aa1ad677dd
Leonardo Firmo <leonardofirmodamota@gmail.com>
2022-08-26 15:44:39 -0300

    Testes com Eclipse no site do senai

---
+++ src/test/java/testeSenaiCursos/TesteBuscarCursos.java
@@ -0,0 +1,36 @@
+package testeSenaiCursos;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteBuscarCursos {
+ private WebDriver driver;
+ 
+ @Before
+ public void abrirNavegador () {
+	 System.setProperty("webdriver.chrome.driver", "C:\\Users\\Leo\\chromedriver\\chromedriver.exe");
+	 driver = new ChromeDriver();
+	 driver.manage().window().maximize();
+ }
+ 
+ @Test
+ public void testeNavegador() {
+	 driver.get("https://informatica.sp.senai.br/");
+	 driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+	 driver.findElement(By.id("Busca1_txtFiltro")).sendKeys("gestão");
+	 driver.findElement(By.id("Busca1_btnBusca")).click();
+ 
+	 String[]listaCurso= {"Gestão","Administração","TI","Robotica"};
+	 
+	
+	 
+	 }
+ 
+	 
+ } 
+
